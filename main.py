c50030973e96ea786f72a8d78b309a9233a18e85
Irfandy <mochammadirfandhy04@gmail.com>
2021-10-25 20:39:52 +0700

    second comit

---
+++ main.py
@@ -0,0 +1,16 @@
+print('Nama : MOCHAMMAD IRFANDHY')
+print('NIM  : 312110126')
+
+nama = str(input('Enter name : '))
+namap = str(input('Enter nickname  : '))
+npm =int(input('Enter your NPM : '))
+umur =int(input('Enter your age : '))
+tempat =str(input('Place of birth  : '))
+alamat = str(input('Your address : '))
+nohp = int(input('Enter phone number : '))
+
+print('Assalamualaikum,')
+print('Let me introduce My self. My name is', nama,'but you can call me',namap+'.')
+print('My NPM is',npm,'I was born in',tempat,'and i am',umur,'years old.')
+print('I am very  glad if you want to invite my house in',alamat,'.So, dont forget to call me before with the number',nohp,'.') 
+print('Thank you.')
